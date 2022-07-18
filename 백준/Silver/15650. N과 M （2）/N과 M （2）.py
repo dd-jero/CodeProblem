d81a786fa6b015a1904136167cd2e62ef2efaf44
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2022-07-18 16:25:38 +0900

    Update N과 M （2）.py

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -6,7 +6,7 @@ n = int(n)
 m = int(m)
 
 list_n = [str(i) for i in range(1,n+1)]
-res = (list(map(' '.join,itertools.combinations(list_n, m))))
-
+res = (list(map(' '.join,itertools.combinations(list_n, m)))) # N과 M (1) 과의 유일한 차이점 : combinations 함수를 사용
+                                                              # combinations 함수는 정렬된 순서를 가지며, 반복되는 요소가 없움
 for i in res:
     print(i)
