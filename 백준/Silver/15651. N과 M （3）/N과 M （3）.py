ec06155174e845afd2823d459427455137047dc6
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2022-07-18 17:05:28 +0900

    Update N과 M （3）.py

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -6,7 +6,7 @@ n = int(n)
 m = int(m)
 
 list_n = [str(i) for i in range(1,n+1)]
-res = (list(map(' '.join,itertools.product(list_n, repeat = m))))
-
+res = (list(map(' '.join,itertools.product(list_n, repeat = m)))) # product 함수와 repeat 키워드를 이용하여 중복 순열 생성 
+                                                                  # 데카르트 곱 ( 곱집합 )
 for i in res:
     print(i)
