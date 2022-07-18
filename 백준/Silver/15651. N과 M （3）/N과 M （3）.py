ad2c59a5b167e17d88f08aa9583e90047300e15d
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2022-07-18 17:01:31 +0900

    [Silver III] Title: N과 M (3), Time: 596 ms, Memory: 88556 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,12 @@
+import sys
+import itertools
+
+n,m = sys.stdin.readline().split()
+n = int(n)
+m = int(m)
+
+list_n = [str(i) for i in range(1,n+1)]
+res = (list(map(' '.join,itertools.product(list_n, repeat = m))))
+
+for i in res:
+    print(i)
