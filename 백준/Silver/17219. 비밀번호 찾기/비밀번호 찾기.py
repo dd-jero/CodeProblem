06b3798721b8ecf8f78ed770a13aecb754dbaf93
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2022-07-24 15:57:24 +0900

    Update 비밀번호 찾기.py

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -1,3 +1,4 @@
+# 이중 for문을 이용하면 무조건 시간초과 -> 딕셔너리를 이용 (Key를 site, Value를 Password)
 import sys
 n,m = map(int, sys.stdin.readline().split())
 list_sp = {}
@@ -9,7 +10,7 @@ for i in range(n):
     list_sp[site] = password
 
 for _ in range(m):
-    list_ans.append(list_sp[input().rstrip()])
+    list_ans.append(list_sp[sys.stdin.readline().rstrip()]) # 입력 받을 때 입력 끝에 개행 문자가 포함되어 rstrip 함수로 제거
 
 for i in list_ans:
     print(i)
