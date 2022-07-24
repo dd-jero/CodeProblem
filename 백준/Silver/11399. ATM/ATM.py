e1b56471821c1daaba3a4480c37bf54bdb97b208
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2022-07-24 16:35:47 +0900

    Update ATM.py

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,8 +1,8 @@
 n = int(input())
 list_time = list(map(int,input().split()))
-list_time = sorted(list_time)
-all_time = 0
-res = 0
+list_time = sorted(list_time)                   # sorted를 통해 시간을 오름차순으로 정렬
+all_time = 0                                    # 각 사람이 돈을 인출하는데 필요한 시간
+res = 0                                         # 누적 합
 for i in range(n):
     all_time += list_time[i]
     res += all_time
