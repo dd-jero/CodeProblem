e6e54f679100ada06283f1069d6d36493eee2d55
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2022-07-28 16:48:19 +0900

    Update 종이의 개수.py

--- 백준/Silver/1780. 종이의 개수/종이의 개수.py
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.py
@@ -5,11 +5,11 @@ def find(paper, num):
     temp_paper = []
 
     for i in paper:
-        temp_paper.extend([*i])
+        temp_paper.extend([*i])                                                         # extend함수를 이용하여 요소들을 한번에 리스트에 추가 
 
-    temp_paper = set(temp_paper)
+    temp_paper = set(temp_paper)                                                        # set 타입으로 변환하여 중복 값 제거
 
-    if len(temp_paper) == 1:
+    if len(temp_paper) == 1:                                                            # 종이의 모든 요소가 동일할 때 개수 증가 
 
         if -1 in temp_paper:
             res[0] += 1
@@ -21,7 +21,7 @@ def find(paper, num):
             res[2] += 1
             return
 
-    else:
+    else:                                                                               # 종이의 모든 요소가 동일하지 않을 때 분할 정복 및 재귀
         div_paper = [paper[i][0:num//3] for i in range(0, num//3)]
         find(div_paper, num//3)
         div_paper = [paper[i][0:num//3] for i in range(num//3, 2*(num//3))]
