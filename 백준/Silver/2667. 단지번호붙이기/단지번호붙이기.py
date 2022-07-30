9d64128f1cda6b0f2b592ccc585c2377c97874dc
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2022-07-31 02:11:24 +0900

    Update 단지번호붙이기.py

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -2,24 +2,24 @@ import sys
 from collections import deque
 
 def find_home(i,j):
-    count = 1
+    count = 1                                                                               
     deq = deque()
     deq.append([i,j])
     home[i][j] = 0
 
     while deq:
         
-        x,y = deq.popleft()
+        x,y = deq.popleft()                                                                 # 이용하는 좌표 제거
 
-        for dx, dy in dxy:
+        for dx, dy in dxy:                                                                  # 상하좌우 인접 좌표 확인
             
             next_x = x + dx
             next_y = y + dy
 
-            if 0 <= next_x < n and 0 <= next_y < n and home[next_x][next_y] == 1:
-                deq.append([next_x,next_y])
-                home[next_x][next_y] = 0
-                count += 1
+            if 0 <= next_x < n and 0 <= next_y < n and home[next_x][next_y] == 1:           # 인접 좌표가 범위 안에 존재하고 좌표의 요소가 1일 때 
+                deq.append([next_x,next_y])                                                 # deque에 추가 (인접 좌표의 인접 좌표가 1일 수도 있어서)
+                home[next_x][next_y] = 0                                                    # 인접 좌표의 요소를 0으로 바꿈
+                count += 1                                                                  # 개수 1 증가
 
     return count
 
@@ -29,15 +29,15 @@ home = []
 for i in range(n):
     home.append(list(map(int,sys.stdin.readline().rstrip())))
 
-cnt = []
+cnt = []                                                                                    # 단지 개수와 단지의 home 개수를 위한 리스트
 
-for i in range(n):
+for i in range(n):                                                                          # 이중 for문 이용
     for j in range(n):
-        if home[i][j] == 1:
-            cnt.append(find_home(i,j))
+        if home[i][j] == 1:                                                                 # 좌표의 요소가 1일 때
+            cnt.append(find_home(i,j))                                                      # BFS 이용하여 home 개수를 구하고 리스트에 추가 
             
-cnt.sort()
-print(len(cnt))
+cnt.sort()                                                                                  # 오름차순으로 정렬          
+print(len(cnt))                                                                             # 단지 개수 
 
-for i in cnt:
+for i in cnt:                                                                               # 단지의 home 개수
     print(i)
