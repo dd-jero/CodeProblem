b31149664f0661f44a4a825667c47baf7e2edb60
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2022-07-31 02:18:09 +0900

    Update 2×n 타일링.py

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,9 +1,10 @@
 dp = [0] * 1001
 dp[1] = 1
 dp[2] = 2
+
 n = int(input())
 
-for i in range(3, n+1):
-    dp[i] = (dp[i-1] + dp[i-2]) % 10007
+for i in range(3, n+1):                             
+    dp[i] = (dp[i-1] + dp[i-2]) % 10007         # 점화식을 이용한 가능 개수 구하기 
 
 print(dp[n])
