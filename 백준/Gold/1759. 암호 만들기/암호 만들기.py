e10a487445768ba95f51d594f6914762f3ccd50d
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2022-08-02 15:27:06 +0900

    Update 암호 만들기.py

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,16 +1,16 @@
-from itertools import combinations
+from itertools import combinations                          # 조합을 이용
 
 def make_pw():
     global collection
 
-    for c in list(combinations(list_c, L)):
+    for c in list(combinations(list_c, L)):                 # L개의 원소를 가지는 조합 구성 
         count_vowel = 0
-        for i in collection:
-            if i in c:
-                count_vowel += 1
+        for i in collection:                                # 모음 리스트 이용
+            if i in c:                                      
+                count_vowel += 1                            # 모음이 존재하면 카운트 
             
-        if count_vowel > 0 and (L-count_vowel) >= 2:
-            res.append("".join(c))
+        if count_vowel > 0 and (L-count_vowel) >= 2:        # 모음 개수를 제외한 요소의 개수가 2 이상일 때 
+            res.append("".join(c))                          # 띄어쓰기 없이 리스트에 추가
 
 
 L,C = map(int,input().split())
