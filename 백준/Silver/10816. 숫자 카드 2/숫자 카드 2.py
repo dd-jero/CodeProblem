1b981caf84d9fff11ac5f2542a594dc97a5ead91
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2022-08-04 14:49:02 +0900

    Update 숫자 카드 2.py

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -10,13 +10,13 @@ count = {}
 for i in own:
     if i in count:
         count[i] += 1
-    else:
+    else:                                               # 딕셔너리에 없으면 value를 1로 가지는 key 값 생성
         count[i] = 1
 
 for i in num:
-    res = count.get(i)
+    res = count.get(i)                                  # 출력하고자 하는 key의 value 값을 추출
 
-    if res == None:
-        print(0, end = " ")
-    else:
+    if res == None:                                     # num에 없으면 0 출력
+        print(0, end = " ") 
+    else:                                               # num에 있으면 value 출력
         print(res, end = " ")
