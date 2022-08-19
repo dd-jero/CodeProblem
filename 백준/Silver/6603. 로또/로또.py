20275587ec3c504ef93b23d989640661c5130866
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2022-08-19 18:19:15 +0900

    Update 로또.py

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -7,10 +7,10 @@ while(1):
     if li[0] == 0:                                                  # 입력의 제일 첫 정수가 0이면 종료
         break
 
-    li.pop(0)                                                       # 제일 첫 정수
+    li.pop(0)                                                       # 제일 첫 정수 제거 
 
-    for j in list(combinations(li, 6)):
-        print(*j, end= "\n")
+    for j in list(combinations(li, 6)):                             # 제거된 리스트를 이용해서 조합
+        print(*j, end= "\n")                                        # 각 테스트 케이스 사이에 한 줄 씩 띄우기 위함
         
-    sys.stdout.write("\n")
+    sys.stdout.write("\n")                                          # print 함수는 뒤에 개행 문자가 포함, sys.stdout,write 함수는 뒤에 개행 문자가 미 포함하여 출력 
 
