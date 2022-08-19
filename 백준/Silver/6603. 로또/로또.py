895b9001e6bf2ad27ed0eb3f627e7794b0af81c5
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2022-08-19 17:31:57 +0900

    Update 로또.py

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -4,10 +4,10 @@ from itertools import combinations
 while(1):
     li = list(map(int, sys.stdin.readline().rstrip().split()))
 
-    if li[0] == 0:
+    if li[0] == 0:                                                  # 입력의 제일 첫 정수가 0이면 종료
         break
 
-    li.pop(0)
+    li.pop(0)                                                       # 제일 첫 정수
 
     for j in list(combinations(li, 6)):
         print(*j, end= "\n")
