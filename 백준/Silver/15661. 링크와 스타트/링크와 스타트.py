a4ef58856b1517151d839975a4b46a8b80fcbc4b
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2022-08-22 17:06:04 +0900

    [Silver I] Title: 링크와 스타트, Time: 1468 ms, Memory: 360412 KB -BaekjoonHub

---
+++ 백준/Silver/15661. 링크와 스타트/링크와 스타트.py
@@ -0,0 +1,37 @@
+from itertools import combinations
+import sys
+
+n = int(sys.stdin.readline())
+t = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
+m = [i for i in range(n)]
+mem = []
+gap = 1899
+
+for i in range(1, n):
+    for team in list(combinations(m, i)):
+        mem.append(list(team))
+
+for i in range(len(mem)//2):
+    stat_1 = 0
+    team_1 = mem[i]
+    
+    for j in range(len(team_1)):
+        member = team_1[j]
+
+        for k in team_1:
+            stat_1 += t[member][k]
+
+    team_2 = mem[-i-1]
+    stat_2 = 0
+
+    for j in range(len(team_2)):
+        member = team_2[j]
+
+        for k in team_2:
+            stat_2 += t[member][k]
+
+    gap = min(gap, abs(stat_1-stat_2))
+
+print(gap)
+
+
