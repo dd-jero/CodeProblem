05cacdc7439bab791be4e586ed60a3230ef8a7d5
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2022-08-24 14:49:00 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 268 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.py
@@ -0,0 +1,9 @@
+n = int(input())
+p = [0] + list(map(int,input().split()))
+dp = [0] + [p[n]]*1000
+
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        dp[i] = min(dp[i], dp[i-j]+p[j])
+
+print(dp[n])
