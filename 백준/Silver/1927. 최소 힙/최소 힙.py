20170a900deefb085762108137d90b5dc49cd6e9
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2022-08-30 17:26:50 +0900

    Update 최소 힙.py

--- 백준/Silver/1927. 최소 힙/최소 힙.py
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -9,14 +9,14 @@ while(1):
 
     x = int(sys.stdin.readline())
 
-    if x != 0:
+    if x != 0:                                  # 0이 아니면 리스트에 요소 추가
         heapq.heappush(li,x)
 
     else:
-        if len(li) != 0:
-            num = heapq.heappop(li)
+        if len(li) != 0:                        # 리스트에 요소가 있으면
+            num = heapq.heappop(li)             # 리스트 최소 값 추출
             print(num)
-        else:
-            print(0)
+        else:                                   # 리스트에 요소가 없으면
+            print(0)                            # 0 출력
 
     n-=1
