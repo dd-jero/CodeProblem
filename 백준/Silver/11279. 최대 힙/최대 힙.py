9a4aa3060a7453b768c1ee7e2f0f9951449bade5
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2022-08-30 17:29:46 +0900

    Update 최대 힙.py

--- 백준/Silver/11279. 최대 힙/최대 힙.py
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -10,11 +10,11 @@ while(1):
     x = int(sys.stdin.readline())
 
     if x != 0:
-        heapq.heappush(li,-x)
+        heapq.heappush(li,-x)                       # 자연수를 음수로 바꾸어 heap 추가 
 
     else:
         if len(li) != 0:
-            num = heapq.heappop(li)*(-1)
+            num = heapq.heappop(li)*(-1)            # 최소 값을 pop하여 -를 곱하면 최대 값
             print(num)
         else:
             print(0)
