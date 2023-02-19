7b482f31af191ab12551fd4a75b93f40a4908d76
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2023-02-19 14:30:49 +0900

    Update 콜라츠 추측.py

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -2,16 +2,14 @@ def solution(num):
     cnt = 0
     while(1):
         if num == 1:
-            return 0
+            return cnt
         elif num % 2 == 0:
             num = num // 2
         else:
             num = num * 3 + 1
         
         cnt += 1
-        
-        if num == 1:
-            return cnt
+    
         
         if cnt == 500:
             return -1
