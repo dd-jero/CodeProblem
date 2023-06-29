e8cc2cb8296a8c46c16c220a0ca99a730efcfe14
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2023-06-29 14:10:18 +0900

    [Silver II] Title: DFS와 BFS, Time: 328 ms, Memory: 39660 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,38 +1,40 @@
-from collections import deque                           # deque 라이브러리를 이용하는 것이 성능 UP
+from collections import deque
 
-def dfs(start):                                         # 깊이 우선 탐색
-    print(start, end = " ")                             # 첫 node 출력
-    visited1[start] = 1                                 # 방문 체크
+def dfs(number, start):
+    print(start, end = " ")  
+    visited[start] = 1
 
-    for i in range(1, n+1):                             
-        if visited1[i] == 0 and s[start][i] == 1:       # 방문하지 않고 연결된 node가 있으면
-            dfs(i)                                      # 재귀
+    for i in range(1,number+1):
+        if connected[start][i] == 1 and not visited[i]:
+            dfs(number,i) 
 
-def bfs(start):                                         # 너비 우선 탐색
+def bfs(number, start):
+    queue = deque()
+    queue.append(start)
     visited2[start] = 1
-    deq = deque()                                       # deque 정의
-    deq.append(start)                                   # deque에 node 추가 
 
-    while deq:                                          # deque에 node가 있을 동안 반복
-        node = deq.popleft()                            # 제일 왼쪽의 node를 추출 후 출력 
-        print(node, end=" ")
+    while queue:
+        temp = queue.popleft()
+        print(temp, end = " ")
 
-        for i in range(1,n+1):
-            if visited2[i] == 0 and s[node][i] == 1:    # 방문하지 않고 연결된 node가 있으면 
-                visited2[i] = 1                         # 방문 체크
-                deq.append(i)                           # deque에 요소로 추가
+        for i in range(1, number+1):
+            if connected[temp][i] == 1 and not visited2[i]:
+                queue.append(i)
+                visited2[i] = 1
 
 
 n,m,v = map(int, input().split())
-visited1 = [0 for _ in range(n+1)]                      # 해당 node를 거쳤는지 확인을 위한 리스트 
+
+connected = [[0]*(n+1) for _ in range(n+1)]
+visited = [0 for _ in range(n+1)]
 visited2 = [0 for _ in range(n+1)]
-s = [[0]*(n+1) for _ in range(n+1)]                     # node 간의 연결 확인을 위한 이중 리스트
 
-for i in range(m):
+for _ in range(m):
     x,y = map(int, input().split())
-    s[x][y] = 1                                         # 이중 리스트의 index를 이용하여 node 간의 연결 구성
-    s[y][x] = 1
+    connected[x][y] = 1
+    connected[y][x] = 1
 
-dfs(v)
+dfs(n,v)
 print()
-bfs(v)
+bfs(n,v)
+
