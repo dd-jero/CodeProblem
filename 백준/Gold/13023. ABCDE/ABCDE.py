e10d251d508645fa782ba89f7b4afbb2e33ebc88
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2023-07-01 01:55:04 +0900

    Update ABCDE.py

--- 백준/Gold/13023. ABCDE/ABCDE.py
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -4,20 +4,20 @@ def solution(n, depth):
     
     global ans
     
-    if depth == 4:
+    if depth == 4: # depth가  4면 함수 종료 
         ans = 1
         return 
     
-    visited[n] = 1
+    visited[n] = 1 # 현재 정점인 n 방문 
     
-    for next in adj[n]:
-        if not visited[next]:
-            solution(next, depth+1)
-            visited[next] = 0
+    for next in adj[n]: # n의 인접 정접을 차례로 방문하기 위함
+        if not visited[next]: # 인접 정점을 방문하지 않았으면 
+            solution(next, depth+1) # 재귀 호출(인접 정점을 현재 정점으로 하고  depth 1 증가)
+            visited[next] = 0 # 재귀 종료되었을 때 해당 정점의 방문 여부를 되돌려 놓아야 다시 방문가능 
 
 n,m = map(int, sys.stdin.readline().split())
-adj = [[] for _ in range(n)]
-ans = 0
+adj = [[] for _ in range(n)] # 각 정점의 인정정점을 저장할 리스트
+ans = 0 # 출력 결정
 
 for _ in range(m):
     a,b = map(int, sys.stdin.readline().split())
@@ -26,11 +26,11 @@ for _ in range(m):
 
 for i in range(n):
     depth = 0
-    visited= [0 for _ in range(n)]
+    visited= [0 for _ in range(n)] # 방문 여부
     
     solution(i, depth)
     
-    if ans == 1:
+    if ans == 1:    # depth가 4면 1을 출력하고 프로그램 종료
         print(1)
         exit()
 
