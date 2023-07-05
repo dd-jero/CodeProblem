0defff8f8d1135a1af931e31c69636c9de1a4ca2
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2023-07-05 19:32:23 +0900

    Update 빙산.py

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,6 +1,8 @@
 import sys
 from collections import deque
 
+# Python3로는 시간 초과, pypy3로 성공
+
 def solution(i,j):
     stack = deque()
     stack.append([i,j])
@@ -19,10 +21,10 @@ def solution(i,j):
                     stack.append([next_x,next_y])
                     visited[next_x][next_y] = 1
 
-                if north[next_x][next_y] == 0:
+                if north[next_x][next_y] == 0: # 인접 요소가 0이면 
                     zero_cnt += 1
 
-        zero[x][y] = zero_cnt
+        zero[x][y] = zero_cnt 
 
 N,M = map(int, sys.stdin.readline().split())
 
@@ -34,7 +36,7 @@ north =[list(map(int,sys.stdin.readline().split())) for _ in range(N)]
 
 while 1:
     visited = [[0 for _ in range(M)] for _ in range(N)]
-    zero = [[0 for _ in range(M)] for _ in range(N)]
+    zero = [[0 for _ in range(M)] for _ in range(N)] # 빙산이 녹는 것을 적용하기 위한 리스트
     union_cnt = 0
 
     for i in range(1, N-1):
@@ -43,18 +45,19 @@ while 1:
                 solution(i,j)
                 union_cnt += 1
     
+    # 빙산을 녹인다
     for x in range(1,N-1):
         for y in range(1, M-1):
-            north[x][y] = max(0,north[x][y]-zero[x][y]) 
+            north[x][y] = max(0,north[x][y]-zero[x][y]) # 최소 0
             
-            if north[x][y] > 0:
+            if north[x][y] > 0: # 빙산이 모두 녹았음을 확인하기 위함
                 check = 1
 
-    if check == 0:
+    if check == 0: # 분리하지 않고 녹았으면
         print(0)
         exit()
 
-    if union_cnt > 1:
+    if union_cnt > 1: # 분리 되었으면 
         print(year)
         exit()
     elif union_cnt < 1:
