db6016bc8284fa10790eb42c0361fc82d7ec681a
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2023-08-16 16:41:47 +0900

    Update 알고스팟.py

--- 백준/Gold/1261. 알고스팟/알고스팟.py
+++ 백준/Gold/1261. 알고스팟/알고스팟.py
@@ -2,6 +2,8 @@ import sys
 from collections import deque
 INF = sys.maxsize
 
+# 0-1 BFS 활용 => 가중치가 0 또는 1일 때만 활용가능
+# visited는 다익스트라의 dist 같은 형태
 def solution():
     q=deque()
     q.append([0,0])
@@ -11,7 +13,7 @@ def solution():
     while q:
         x,y= q.popleft()
 
-        if x == n-1 and y == m-1:
+        if x == n-1 and y == m-1: # 마지막 위치이면 종료 -> 가중치 0일 때는 큐의 앞에 삽입하여 최소 갯수 출력
             return
         
         for dx,dy in dxy:
@@ -21,9 +23,10 @@ def solution():
             if 0<=nxt_x<n and 0<=nxt_y<m:
                 if visited[x][y] + int(miro[nxt_x][nxt_y]) < visited[nxt_x][nxt_y]:
                     visited[nxt_x][nxt_y] = visited[x][y] + int(miro[nxt_x][nxt_y]) 
-                    if miro[nxt_x][nxt_y] == '0':
+
+                    if miro[nxt_x][nxt_y] == '0': # 가중치 0일 때는 큐의 앞에 삽입
                         q.appendleft([nxt_x,nxt_y])
-                    if miro[nxt_x][nxt_y] == '1':
+                    if miro[nxt_x][nxt_y] == '1': # 가중치 1일 때는 큐의 뒤에 삽입
                         q.append([nxt_x,nxt_y])
                         
    
