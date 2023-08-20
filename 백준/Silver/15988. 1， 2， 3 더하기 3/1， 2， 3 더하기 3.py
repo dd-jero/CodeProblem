5b73352678083b3f4d0ffd55b9829efd622ab158
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2023-08-20 13:44:57 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 520 ms, Memory: 70812 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
@@ -4,8 +4,8 @@ dp = [0,1,2,4] +[0]*999997
 t = int(sys.stdin.readline().strip())
 
 for i in range(4,1000001):
-    dp[i] = dp[i-1]%1000000009 + dp[i-2]%1000000009 + dp[i-3]%1000000009
+    dp[i] = (dp[i-1]%1000000009 + dp[i-2]%1000000009 + dp[i-3]%1000000009)%1000000009
 
 for _ in range(t):
     num = int(sys.stdin.readline().strip())
-    print(dp[num]%1000000009)
+    print(dp[num])
