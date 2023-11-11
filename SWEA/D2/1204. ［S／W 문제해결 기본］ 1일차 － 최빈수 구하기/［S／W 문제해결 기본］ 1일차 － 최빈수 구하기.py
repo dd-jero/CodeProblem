4ff71d8e84ebc473fadd0457cd869cbe3c73807a
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2023-11-11 12:34:57 +0900

    Update ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.py

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.py
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.py
@@ -4,10 +4,11 @@ for test_case in range(1, T + 1):
     # ///////////////////////////////////////////////////////////////////////////////////
     tc = int(input())
     num_list = list(map(int, input().split()))
-    dic = {i:0 for i in range(0,100+1)}
+    dic = {i:0 for i in range(0,100+1)} #dictionary 사용 (점수는 0 ~ 100 범위임)
 
     for num in num_list:
         dic[num] += 1
-    max_list = [k for k,v in dic.items() if max(dic.values()) == v]
+        
+    max_list = [k for k,v in dic.items() if max(dic.values()) == v] # max의 value를 가지는 key들을 리스트에 저장 
     print(f"#{tc} {max(max_list)}")
     # ///////////////////////////////////////////////////////////////////////////////////
