e00c29cd7bc3c1745583197f28c1bbbe782ce2e7
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2024-02-07 21:05:24 +0900

    [Silver IV] Title: 색종이 - 2, Time: 80 ms, Memory: 11576 KB -BaekjoonHub

--- 백준/Silver/2567. 색종이 － 2/색종이 － 2.java
+++ 백준/Silver/2567. 색종이 － 2/색종이 － 2.java
@@ -8,7 +8,7 @@ import java.util.StringTokenizer;
 public class Main {
 	
 	static int N;
-	static int[][] check;
+	static boolean[][] check;
 	static int[] dx = {0,-1,0,1};
 	static int[] dy = {1,0,-1,0};
 	
@@ -17,7 +17,7 @@ public class Main {
 		StringTokenizer st;
 		
 		N= Integer.parseInt(br.readLine());
-		check = new int[101][101];
+		check = new boolean[101][101];
 		int total = 0;
 		
 		for(int i=0;i<N;i++) {
@@ -25,28 +25,21 @@ public class Main {
 			int X = Integer.parseInt(st.nextToken());
 			int Y = Integer.parseInt(st.nextToken());
 			
-			for(int j=X;j<X+10;j++) {
+			for(int j=X;j<X+10;j++) { // 색종이의 모든 부분은 true 설정 
 				for(int k=Y;k<Y+10;k++) {
-					check[j][k] = 1;
+					check[j][k] = true;
 				}
 			}
 		}
 		
-//		for(int i=0;i<101;i++) {
-//			for(int j=0;j<101;j++) {
-//				System.out.print(check[i][j]);
-//			}
-//			System.out.println();
-//		}
-		
-		for(int i=0;i<=100;i++) {
-			for(int j=0;j<=100;j++) {
-				if(check[i][j]==0) {
+		for(int i=0;i<101;i++) {
+			for(int j=0;j<101;j++) {
+				if(!check[i][j]) { // false인 위치 일 때
 					for(int d=0;d<4;d++) {
 						int nx = i + dx[d];
 						int ny = j + dy[d];
 						if(-1<nx&&nx<101&&-1<ny&&ny<101) {
-							if(check[nx][ny] == 1) {
+							if(check[nx][ny]) { // 상하좌우 중 하나라도 true 있으면 카운트 증가 
 								total++;
 							}
 						}
