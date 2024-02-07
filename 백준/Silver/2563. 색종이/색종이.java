222691c6155cc32fda0ff23c208e79bc88db7421
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2024-02-07 19:45:59 +0900

    [Silver V] Title: 색종이, Time: 120 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int N, total;
+	static boolean[][] check = new boolean[101][101];
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		N = Integer.parseInt(br.readLine());
+		
+		for(int i=0;i<N;i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int X = Integer.parseInt(st.nextToken());
+			int Y = Integer.parseInt(st.nextToken());
+			
+			for(int j=X;j<X+10;j++) {
+				for(int k=Y;k<Y+10;k++) {
+					if(check[j][k]) continue;
+					check[j][k] = true;
+				}
+			}
+		}
+		
+		for(int j=0;j<101;j++) {
+			for(int k=0;k<101;k++) {
+				if(check[j][k]) total++;
+			}
+		}
+		
+		bw.write(String.valueOf(total));
+		
+		bw.flush();
+		
+		br.close();
+		bw.close();
+	}
+}
