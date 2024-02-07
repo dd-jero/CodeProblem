6044f4c15657771410e11872a077064d9c1277f1
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2024-02-07 19:49:12 +0900

    [Silver V] Title: 색종이, Time: 80 ms, Memory: 11680 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -8,6 +8,7 @@ import java.util.StringTokenizer;
 public class Main {
 	
 	static int N, total;
+	static int X,Y;
 	static boolean[][] check = new boolean[101][101];
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -20,27 +21,21 @@ public class Main {
 		for(int i=0;i<N;i++) {
 			st = new StringTokenizer(br.readLine());
 			
-			int X = Integer.parseInt(st.nextToken());
-			int Y = Integer.parseInt(st.nextToken());
+			X = Integer.parseInt(st.nextToken());
+			Y = Integer.parseInt(st.nextToken());
 			
 			for(int j=X;j<X+10;j++) {
 				for(int k=Y;k<Y+10;k++) {
 					if(check[j][k]) continue;
 					check[j][k] = true;
+					total++;
 				}
-			}
-		}
-		
-		for(int j=0;j<101;j++) {
-			for(int k=0;k<101;k++) {
-				if(check[j][k]) total++;
 			}
 		}
 		
 		bw.write(String.valueOf(total));
 		
 		bw.flush();
-		
 		br.close();
 		bw.close();
 	}
