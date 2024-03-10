f9c37255610040d90657d2dd55aec374824601cd
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2024-03-10 12:23:38 +0900

    [Unrated] Title: [모의 SW 역량테스트] 탈주범 검거, Time: 133 ms, Memory: 23,568 KB -BaekjoonHub

--- SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
+++ SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
@@ -43,8 +43,19 @@ class Solution{
 				}
 			} // 입력 완료 
 			
-			res = 1; 
 			bfs(); 
+			
+			res = 0; 
+			
+			// 방문 체크 개수 확인
+			for(int i=0;i<N;i++) {
+				for(int j=0;j<M;j++) {
+					if(visited[i][j]) {
+						res++;
+					}
+				}
+			}
+			
 			
 			sb.append("#"+t+" "+res+"\n");
 			
@@ -125,7 +136,6 @@ class Solution{
 				
 				q.offer(new int[] {x,y-1});
 				visited[x][y-1] = true;
-				res++;
 			}
 		}
 		
@@ -142,7 +152,6 @@ class Solution{
 				
 				q.offer(new int[] {x,y+1});
 				visited[x][y+1] = true;
-				res++;
 			}
 		}
 	}
@@ -157,7 +166,6 @@ class Solution{
 				
 				q.offer(new int[] {x - 1,y});
 				visited[x-1][y] = true;
-				res++;
 			}
 		}
 	}
@@ -172,7 +180,6 @@ class Solution{
 				
 				q.offer(new int[] {x+1,y});
 				visited[x+1][y] = true;
-				res++;
 			}
 		}
 	}
