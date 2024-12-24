768f90ed4a66a873a21dd8585c01021fb20c9fbc
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2024-12-24 20:40:29 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,65 +1,42 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <vector>
-#include <algorithm> // sort 메서드 위해
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int arr[9];
-vector<int> vec;
-void recur(int idx, vector<int> memnbers); // 함수의 원형을 반드시 맨 위에 선언해주어야함!
+int main() {
+    int heights[9];
+    int total_sum = 0;
 
-int main(void) {
+    // 키 입력 및 전체 합 계산
+    for (int i = 0; i < 9; i++) {
+        cin >> heights[i];
+        total_sum += heights[i];
+    }
 
-	ios::sync_with_stdio(false);
-	cin.tie(0);
+    // 이중 반복문으로 2명의 키 찾기
+    for (int i = 0; i < 8; i++) {
+        for (int j = i + 1; j < 9; j++) {
+            // 전체 합에서 2명의 키를 뺐을 때 100이 되면
+            if (total_sum - heights[i] - heights[j] == 100) {
+                // 정렬을 위한 임시 배열
+                vector<int> result;
+                for (int k = 0; k < 9; k++) {
+                    if (k != i && k != j) {
+                        result.push_back(heights[k]);
+                    }
+                }
 
-	// 입력 
-	for (int i = 0;i < 9;i++) {
-		//scanf("%d", &arr[i]);
-		cin >> arr[i];
-	}
-	
-	// 재귀 시작 
-	// 파라미터: 인덱스, 현재 고려한 난쟁이들의 키 벡터 
-	recur(0, vec);
+                // 오름차순 정렬
+                sort(result.begin(), result.end());
 
-	return 0; 
-}
+                // 7명의 키 출력
+                for (int num : result) {
+                    cout << num << '\n';
+                }
 
-void recur(int idx, vector<int> members) {
+                return 0;
+            }
+        }
+    }
 
-	if (members.size() == 7) { // 7명의 난쟁이이면
-			
-		int sum_mem = 0;
-
-		// 키 합 구하기 
-		for (int num : members) {
-			sum_mem += num;
-		}
-
-		if (sum_mem == 100) { // 키의 합이 100이 되면 
-			sort(members.begin(), members.end());
-
-			for (int num : members) {
-				//printf("%d\n", num);
-				cout << num << "\n";
-			}
-			exit(0); // 종료 
-			
-		}
-		else { // 키의 합의 100이 아니면
-			return; // 다시 되돌아감
-		}
-	}
-
-	for (int i = idx; i < 9;i++) {
-
-		members.push_back(arr[i]);
-
-		recur(idx+1, members);
-
-		// 되돌아 왔을 때 마지막 요소 제거 
-		members.pop_back();
-	}
+    return 0;
 }
