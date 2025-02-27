78245914901e3867e4b29d51198a7040cc35c9c8
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2025-02-27 16:07:52 +0900

    [Gold V] Title: 트리와 쿼리, Time: 68 ms, Memory: 22688 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.cc
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.cc
@@ -7,8 +7,7 @@ vector<vector<int>> tree;
 vector<vector<int>> childnode; // i 노드의 child 
 vector<int> subtree_size; // 해당 i 노드의 서브 트리 노드 수
 
-void MakeTree(int cur_node, int parent); 
-void CountSubtreeNodes(int node);
+int MakeTree(int cur_node, int parent); 
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -27,7 +26,6 @@ int main() {
 	}
 
 	MakeTree(R, -1);
-	CountSubtreeNodes(R);
 
 	int q;
 	for (int i = 0;i < Q;i++) {
@@ -39,20 +37,16 @@ int main() {
 	return 0;
 }
 
-void MakeTree(int cur_node, int parent) {
+int MakeTree(int cur_node, int parent) {
+
+	int size = 1; // 현재 노드 포함 
+
 	for (int node : tree[cur_node]) { // 현재 노드와 연결된 노드 확인
 		if (node != parent) { // 부모 노드 아니면
 			childnode[cur_node].push_back(node); // 자식 노드로 삽입
-			MakeTree(node, cur_node); // 현재 노드를 부모로 하는 자식 노드의 트리 구성 
+			size += MakeTree(node, cur_node); // 현재 노드를 부모로 하는 자식 노드들의 개수
 		}
 	}
-}
-
-void CountSubtreeNodes(int node) {
-	subtree_size[node] = 1; // 현재 노드를 포함
-
-	for (int n : childnode[node]) {
-		CountSubtreeNodes(n); // 현재 노드의 자식 노드의 서브 트리 노드 수 구하기 
-		subtree_size[node] += subtree_size[n];
-	}
+	subtree_size[cur_node] = size; // 현재 노드의 자식 노드들의 개수 저장 
+	return size;
 }
