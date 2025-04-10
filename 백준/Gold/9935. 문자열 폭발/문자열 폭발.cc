5fb2a2b5a776bc2025f1977ba8d481707c91495d
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2025-04-10 19:30:12 +0900

    [Gold IV] Title: 문자열 폭발, Time: 12 ms, Memory: 6056 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -1,45 +1,41 @@
 #include <iostream>
+#include <stack>
 #include <string>
 using namespace std;
 
-string str, bomb;
-
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
 
-	cin >> str >> bomb;
-	int len_str = str.length();
-	int len_bomb = bomb.length();
+    string str, bomb;
+    cin >> str >> bomb;
 
-	string tmp= ""; // 한글자씩 추가하면서 폭발 문자열과 확인하기 위함.
-	for(int idx=0;idx < len_str; idx++) {
+    string result;
 
-		tmp += str[idx]; 
-		if (tmp.length() >= len_bomb) { // 폭발 문자열의 길이와 같거나 크면 
+    for (char c : str) {
+        result.push_back(c);
 
-			bool is_bomb = true; // 폭발이 일어날지 여부 
+        // 폭발 문자열과 길이가 같거나 길 때만 검사
+        if (result.size() >= bomb.size()) {
+            bool isMatch = true;
+            for (int i = 0; i < bomb.size(); ++i) {
+                if (result[result.size() - bomb.size() + i] != bomb[i]) {
+                    isMatch = false;
+                    break;
+                }
+            }
 
-			for (int i = 0;i < len_bomb;i++) {
-				if (tmp[tmp.length() - len_bomb + i] != bomb[i]) { // 해당 단어와 폭발 문자열의 단어가 다르면 
-					is_bomb = false;
-					break;
-				}
-			}
+            if (isMatch) {
+                // 폭발 문자열 제거
+                for (int i = 0; i < bomb.size(); ++i) {
+                    result.pop_back();
+                }
+            }
+        }
+    }
 
-			if (is_bomb) { // 폭발 문자열과 같으면 
-				tmp.erase(tmp.length() - len_bomb, len_bomb); // 첫번째 인자의 위치부터 두번째 인자의 길이만큼 제거 
-			}
-		}
-	}
+    if (result.empty()) cout << "FRULA\n";
+    else cout << result << "\n";
 
-	if (tmp.empty()) { /// 남아있는 문자 없는 경우
-		cout << "FRULA" << "\n";
-	}
-	else {
-		cout << tmp << "\n";
-	}
-
-	return 0;
+    return 0;
 }
