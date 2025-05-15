f9eb71191cfdb68ed7a63fe091a1b8d6dc0f7b04
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2025-05-15 17:43:15 +0900

    [Silver I] Title: 나이트의 이동, Time: 24 ms, Memory: 2452 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
@@ -3,25 +3,15 @@
 #include <queue>
 using namespace std;
 
-struct Info {
-	int x;
-	int y;
-	int cnt; // 이동 횟수
-
-	bool operator<(const Info& other) const {
-		return this->cnt > other.cnt; // 이동 횟수 오름차순 
-	}
-};
-
 int T, I; // 테케 수, 체스판 한 변의 길이 
 int init_x, init_y; // 초기 나이트 위치
 int dest_x, dest_y; // 이동하려는 위치 
-vector<vector<bool>> visited; // 방문 체크
+vector<vector<int>> visited; // 해당 위치 접근하는 최소 이동 수 
 
 int dx[8] = { -2,-2,-1,1,2,2,1,-1 };
 int dy[8] = { -1,1,2,2,1,-1,-2,-2 };
 
-int BFS();
+void BFS();
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -31,42 +21,38 @@ int main() {
 
 	for (int t = 0;t < T;t++) {
 		cin >> I;
-		visited.assign(I, vector<bool>(I, false)); // 방문 체크 초기화 
+		visited.assign(I, vector<int>(I, 0)); // 방문 체크 초기화 
 		cin >> init_x >> init_y >> dest_x >> dest_y;
 
-		int res = BFS();
+		BFS();
 
-		cout << res << "\n";
-
-		visited.clear();
-
+		cout << visited[dest_x][dest_y] << "\n";
 	} 
 	return 0;
 }
 
-int BFS() {
-	priority_queue<Info> pq;
+void BFS() {
+	queue<pair<int,int>> q;
 
 	// 나이트의 초기 위치 
-	pq.push({ init_x, init_y, 0 });
-	visited[init_x][init_y] = true;
+	q.push({ init_x, init_y});
+	visited[init_x][init_y] = 0;
 
-	while (!pq.empty()) {
-		auto cur = pq.top();
-		pq.pop();
+	while (!q.empty()) {
+		auto cur = q.front();
+		q.pop();
 
-		if (cur.x == dest_x && cur.y == dest_y) return cur.cnt; // 목표 위치 도착
+		if (cur.first == dest_x && cur.second == dest_y) return; 
 
 		for (int d = 0;d < 8;d++) {
-			int nxt_x = cur.x + dx[d];
-			int nxt_y = cur.y + dy[d];
+			int nxt_x = cur.first + dx[d];
+			int nxt_y = cur.second + dy[d];
 
 			if (nxt_x < 0 || nxt_x >= I || nxt_y < 0 || nxt_y >= I) continue; // 범위 초과
+			if (visited[nxt_x][nxt_y] > 0) continue; // 이전에 방문했으면
 
-			if (!visited[nxt_x][nxt_y]) { // 아직 방문 안했으면 
-				pq.push({ nxt_x,nxt_y,cur.cnt + 1 });
-				visited[nxt_x][nxt_y] = true;
-			}
+			q.push({nxt_x,nxt_y});
+			visited[nxt_x][nxt_y] = visited[cur.first][cur.second] + 1;
 		}
 
 	}
