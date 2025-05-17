10a4cc3933535c228b926ed04ffd3f79f3ae8fb3
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2025-05-17 19:19:42 +0900

    [Silver II] Title: 연속합, Time: 4 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -1,45 +1,38 @@
 #include <iostream>
 using namespace std;
 
-int n, res = 0;
-int arr[100001], dp[100001];
+int N, numbers[100001], res = -1000; // 정수 N개
+int dp[100001];
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0); cout.tie(0);
+	
+	cin >> N;
 
-	cin >> n;
-
-	for (int i = 0;i < n;i++) {
-		cin >> arr[i];
+	for (int i = 0;i < N;i++) {
+		cin >> numbers[i];
 	}
 
-	dp[0] = arr[0];
-	res = arr[0];
-
-	for (int i = 1;i < n;i++) {
-		if (arr[i] < 0) {
-			if (dp[i - 1] + arr[i] > 0) {
-				dp[i] = dp[i - 1] + arr[i];
-			}
-			else {
-				dp[i] = arr[i];
-			}
+	dp[0] = numbers[0];
+	for (int i = 1;i < N;i++) {
+		
+		if (numbers[i] >= 0) { // 현재 요소가 양수면
+			if (dp[i - 1] < 0) dp[i] = numbers[i]; // 이전 최대 합이 음수면 초기화
+			else dp[i] = dp[i - 1] + numbers[i]; // 합에 더하기
 		}
-		else {
-			if (dp[i - 1] < 0) {
-				dp[i] = arr[i];
-			}
-			else {
-				dp[i] = dp[i - 1] + arr[i];
-			}
+		else { // 현재 요소가 음수면 
+			int check = dp[i - 1] + numbers[i];
+			if (check >= 0) dp[i] = check; // 이후 양수를 더해서 더 큰 값이 나올 수 있음.
+			else dp[i] = numbers[i];
 		}
 	}
 
-	for (int i = 1;i < n;i++) {
-		//cout << dp[i] << " ";
+	for (int i = 0;i < N;i++) {
 		res = res < dp[i] ? dp[i] : res;
 	}
-	
+
 	cout << res << "\n";
+
+	return 0;
 }
