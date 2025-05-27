6428eaa6938c8b88907200f0b3261a7d4d993168
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2025-05-27 17:49:45 +0900

    [Gold V] Title: 트리와 쿼리, Time: 72 ms, Memory: 16864 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.cc
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.cc
@@ -3,47 +3,58 @@
 using namespace std;
 
 int N, R, Q; // 트리 정점 수, 루트 번호, 쿼리 수
-vector<vector<int>> tree;
-vector<int> subtree_size; // 해당 i 노드의 서브 트리 노드 수
+vector<vector<int>> connected; // connect[i]: 정점 i와 연결된 정점들
+vector<int> subsize, parent, res; // subsize[i]: 정점 i의 서브트리 정점 개수 
 
-int MakeTree(int cur_node, int parent); 
+int MakeTree(int cur_node, int parent_node);
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0); cout.tie(0);
 
 	cin >> N >> R >> Q;
-	tree.resize(N+1);
-	subtree_size.resize(N + 1, 0);
 
-	int s, e;
-	for (int i = 1;i < N;i++) {
-		cin >> s >> e;
-		tree[s].push_back(e);
-		tree[e].push_back(s);
+	connected.resize(N + 1);
+	parent.resize(N + 1);
+	subsize.resize(N + 1);
+
+	int U, V;
+	for (int i = 0;i < N-1;i++) {
+		cin >> U >> V;
+		connected[U].push_back(V);
+		connected[V].push_back(U);
 	}
 
 	MakeTree(R, -1);
 
-	int q;
+	int root;
+
 	for (int i = 0;i < Q;i++) {
-		cin >> q; // 해당 정점을 루트로
-		
-		cout << subtree_size[q] << "\n";
+		cin >> root;
+		res.push_back(root);
+	}
+
+	for (int i = 0;i < Q;i++) {
+		cout << subsize[res[i]] << "\n";
 	}
 
 	return 0;
 }
 
-int MakeTree(int cur_node, int parent) {
+int MakeTree(int cur_node, int parent_node) {
 
-	int size = 1; // 현재 노드 포함 
+	int size = 1;
 
-	for (int node : tree[cur_node]) { // 현재 노드와 연결된 노드 확인
-		if (node != parent) { // 부모 노드 아니면
-			size += MakeTree(node, cur_node); // 현재 노드를 부모로 하는 자식 노드들의 개수
+	for (int node : connected[cur_node]) { // 현재 정점과 연결된 정점들 확인
+		if (subsize[node] > 0) continue;
+
+		if (node != parent_node) { // 부모가 아니면
+			parent[node] = cur_node; // 해당 정점의 부모는 현재 정점
+			 size += MakeTree(node, cur_node); 
 		}
 	}
-	subtree_size[cur_node] = size; // 현재 노드의 자식 노드들의 개수 저장 
+
+	subsize[cur_node] = size;
+
 	return size;
 }
