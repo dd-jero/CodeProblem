320ee0e8d2bde464a8c24d00d6948d84f124b7d3
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2025-05-27 17:52:03 +0900

    [Gold V] Title: 트리와 쿼리, Time: 56 ms, Memory: 14052 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.cc
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.cc
@@ -4,7 +4,7 @@ using namespace std;
 
 int N, R, Q; // 트리 정점 수, 루트 번호, 쿼리 수
 vector<vector<int>> connected; // connect[i]: 정점 i와 연결된 정점들
-vector<int> subsize, parent, res; // subsize[i]: 정점 i의 서브트리 정점 개수 
+vector<int> subsize; // subsize[i]: 정점 i의 서브트리 정점 개수 
 
 int MakeTree(int cur_node, int parent_node);
 
@@ -15,7 +15,6 @@ int main() {
 	cin >> N >> R >> Q;
 
 	connected.resize(N + 1);
-	parent.resize(N + 1);
 	subsize.resize(N + 1);
 
 	int U, V;
@@ -28,15 +27,11 @@ int main() {
 	MakeTree(R, -1);
 
 	int root;
-
 	for (int i = 0;i < Q;i++) {
 		cin >> root;
-		res.push_back(root);
+		cout << subsize[root] << "\n";
 	}
 
-	for (int i = 0;i < Q;i++) {
-		cout << subsize[res[i]] << "\n";
-	}
 
 	return 0;
 }
@@ -46,10 +41,8 @@ int MakeTree(int cur_node, int parent_node) {
 	int size = 1;
 
 	for (int node : connected[cur_node]) { // 현재 정점과 연결된 정점들 확인
-		if (subsize[node] > 0) continue;
 
 		if (node != parent_node) { // 부모가 아니면
-			parent[node] = cur_node; // 해당 정점의 부모는 현재 정점
 			 size += MakeTree(node, cur_node); 
 		}
 	}
