03c53e1df785a9304706182b9f2465d9b6228ed5
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2025-05-28 18:33:17 +0900

    [Gold III] Title: 텀 프로젝트, Time: 448 ms, Memory: 3192 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
@@ -1,9 +1,9 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int T, N;
-vector<int> want, checked;
+int want[100001], checked[100001]; // vector → 정적 배열
+int path[100001]; // 경로 추적용 배열
 void BFS(int start_student);
 
 int main() {
@@ -15,8 +15,11 @@ int main() {
     for (int i = 0; i < T; i++) {
         cin >> N;
 
-        want.assign(N + 1, 0);
-        checked.assign(N + 1, 0);
+        // 배열 초기화
+        for (int j = 0; j <= N; j++) {
+            want[j] = 0;
+            checked[j] = 0;
+        }
 
         for (int j = 1; j <= N; j++) {
             cin >> want[j];
@@ -40,7 +43,7 @@ int main() {
 }
 
 void BFS(int start_student) {
-    vector<int> prev;
+    int path_size = 0;
     int cur = start_student;
 
     while (true) {
@@ -51,15 +54,15 @@ void BFS(int start_student) {
         if (checked[cur] == 1) { // 현재 경로에서 이미 방문 = 사이클 발견
             // cur부터 사이클 시작점까지 팀 완성
             bool in_cycle = false;
-            for (int student : prev) {
-                if (student == cur) in_cycle = true;
-                if (in_cycle) checked[student] = 2;
+            for (int i = 0; i < path_size; i++) {
+                if (path[i] == cur) in_cycle = true;
+                if (in_cycle) checked[path[i]] = 2;
             }
             return;
         }
 
         checked[cur] = 1; // 현재 경로에서 방문 중 표시
-        prev.push_back(cur);
+        path[path_size++] = cur;
 
         int nxt = want[cur];
         if (cur == nxt) { // 자기 자신 선택
