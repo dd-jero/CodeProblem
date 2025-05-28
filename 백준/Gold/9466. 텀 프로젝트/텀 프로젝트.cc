3681b434b663c60c4f20298eb39c856c8300285f
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2025-05-28 18:04:22 +0900

    [Gold III] Title: 텀 프로젝트, Time: 580 ms, Memory: 3948 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
@@ -1,67 +1,72 @@
 #include <iostream>
-#include <cstring>
+#include <vector>
 using namespace std;
 
-int t, n, res;
-int vec[100001];
-int checked[100001]; // -1: 팀 실패, 0: 아직 확인 안 됨, 1: 팀 성공
-bool visited[100001];
-
-void dfs(int cur_mem);
+int T, N;
+vector<int> want, checked;
+void BFS(int start_student);
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+    cin.tie(0); cout.tie(0);
 
-    cin >> t;
+    cin >> T;
 
-    for (int tc = 0; tc < t; tc++) {
-        cin >> n;
-        res = 0;
+    for (int i = 0; i < T; i++) {
+        cin >> N;
 
-        for (int i = 1; i <= n; i++) {
-            cin >> vec[i];
+        want.assign(N + 1, 0);
+        checked.assign(N + 1, 0);
+
+        for (int j = 1; j <= N; j++) {
+            cin >> want[j];
         }
 
-        for (int i = 1; i <= n; i++) {
-            if (checked[i] == 0) { // 아직 확인되지 않은 경우만 DFS 수행
-                dfs(i);
+        for (int j = 1; j <= N; j++) {
+            if (checked[j] == 0) {
+                BFS(j);
             }
         }
 
-        // 팀에 포함되지 않은 학생 수 계산
-        for (int i = 1; i <= n; i++) {
-            if (checked[i] == -1) res++;
+        int res = 0;
+        for (int j = 1; j <= N; j++) {
+            if (checked[j] == -1) res++;
         }
 
         cout << res << "\n";
-
-        memset(vec, 0, sizeof(vec));
-        memset(checked, 0, sizeof(checked));
-        memset(visited, false, sizeof(visited));
     }
 
     return 0;
 }
 
-void dfs(int cur_mem) {
+void BFS(int start_student) {
+    vector<int> prev;
+    int cur = start_student;
 
-    visited[cur_mem] = true;
-    int nxt_mem = vec[cur_mem];
-
-    if (!visited[nxt_mem]) { // 아직 방문하지 않은 경우
-        dfs(nxt_mem);
-    }
-    else if (checked[nxt_mem] == 0) { // 사이클 발견
-        // 사이클에 포함된 모든 노드 처리
-        for (int temp = nxt_mem; temp != cur_mem; temp = vec[temp]) {
-            checked[temp] = 1;
+    while (true) {
+        if (checked[cur] == 2) { // 이미 팀 완성된 학생
+            return;
         }
-        checked[cur_mem] = 1;
-    }
 
-    if (checked[cur_mem] != 1) { // 사이클에 포함되지 않으면 실패로 처리
-        checked[cur_mem] = -1;
+        if (checked[cur] == -1) { // 현재 경로에서 이미 방문 = 사이클 발견
+            // cur부터 사이클 시작점까지 팀 완성
+            bool in_cycle = false;
+            for (int student : prev) {
+                if (student == cur) in_cycle = true;
+                if (in_cycle) checked[student] = 2;
+            }
+            return;
+        }
+
+        checked[cur] = -1; // 현재 경로에서 방문 중 표시
+        prev.push_back(cur);
+
+        int nxt = want[cur];
+        if (cur == nxt) { // 자기 자신 선택
+            checked[cur] = 2;
+            return;
+        }
+
+        cur = nxt;
     }
 }
