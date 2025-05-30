9cf7363c4dd5dac2d5abd20838aec58750d2d7f0
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2025-05-30 21:06:23 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+char N;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	cin >> N;
+
+	cout << (int)N << "\n";
+
+	return 0;
+}
